51fa58679b8a96cddbc91380a55f555897006d8d
bogdanov <bbogdan_ov@mail.ru>
2025-09-16 21:53:11 +0300

    include necessary headers into `macros.h`

--- src/macros.h
+++ src/macros.h
@@ -1,6 +1,9 @@
 #ifndef MACROS_H
 #define MACROS_H
 
+#include <pthread.h>
+#include <assert.h>
+
 #define LOCK(MUTEX) assert(pthread_mutex_lock(MUTEX) == 0)
 #define TRYLOCK(MUTEX) pthread_mutex_trylock(MUTEX)
 #define UNLOCK(MUTEX) assert(pthread_mutex_unlock(MUTEX) == 0)
