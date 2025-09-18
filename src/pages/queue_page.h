d5379bf6fa302b2db70616579857d2aae53fc52c
bogdanov <bbogdan_ov@mail.ru>
2025-09-18 22:13:05 +0300

    reword comment

--- src/pages/queue_page.h
+++ src/pages/queue_page.h
@@ -38,7 +38,7 @@ typedef struct QueuePage {
 	// Currently reordering entry index
 	// -1 - nothing is being dragged
 	int reordering_idx;
-	// Number of the reordering entry from which it was reordered
+	// Number of the reordered entry from which it was reordered
 	int reordered_from_number;
 	float reorder_click_offset_y;
 
